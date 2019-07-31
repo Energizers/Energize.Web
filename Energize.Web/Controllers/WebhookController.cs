12490f3dffdd76f8cc83960e6858b365c36bd7e7
Earu <earu.derg@gmail.com>
2019-07-31 13:57:01 +0200

    Add a webhook endpoint

---
+++ Energize.Web/Controllers/WebhookController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Energize.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebhookController : ControllerBase
+    {
+        // POST: api/Webhook
+        [HttpPost]
+        public void Post([FromBody] Dictionary<string, string> values)
+        {
+            string json = JsonConvert.SerializeObject(values);
+            Console.WriteLine(json);
+        }
+    }
+}
